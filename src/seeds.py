f207b47d36614485524f4be5f5c57d795d8816b6
Yoyo <yoyowamg1012@gmail.com>
2024-12-01 16:08:23 -0600

    remove water body when initialize

--- src/seeds.py
+++ src/seeds.py
@@ -3,8 +3,6 @@ import numpy as np
 
 def initialize_grid(rows, cols):
     grid = np.random.choice([0, 1], size=(rows, cols), p=[0.3, 0.7])  # 30% 未種植，70% 樹木
-    water_bodies = np.random.choice([0, 3], size=(rows, cols), p=[0.9, 0.1])  # 10% 水體
-    grid = np.maximum(grid, water_bodies)  # 合併水體和地形特徵
     grid[10, 10] = 2  # 火災初始點設為 (10, 10)
 
     # 樹木類型
